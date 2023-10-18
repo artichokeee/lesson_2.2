9bd0cef7b86a1e3d4f8861c5f1186e2e9d7019fa
artichokeee <rusau.qalife@gmail.com>
2023-10-18 22:29:27 +0300

    updated calculations.js for Task 2.2.3.

--- calculations.js
+++ calculations.js
@@ -1,20 +1,27 @@
 function increaseBalance(x, y) {
-  x = x + y;
+  x += y;
   return x;
 }
 
 function decreaseBalance(x, y) {
-  x = x - y;
+  x -= y;
   return x;
 }
 
 function divideBalanceByAccounts(x, y, z) {
-  x = (x + z) / y;
+  x = (x + y) / z;
   return x;
 }
 
+// Через assignment operators можно организовать эту функцию так, но мне кажется, что в этом нет большого смысла
+// function divideBalanceByAccounts(x, y, z) {
+//   x += z;
+//   x /= y;
+//   return x;
+// }
+
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x %= y;
   return x;
 }
 
