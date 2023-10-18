8637c29e1203e69a2d6309379fe8f29aa4558844
artichokeee <rusau.qalife@gmail.com>
2023-10-18 22:09:04 +0300

    fixed calculations.js for Task 2.1.1.

---
+++ calculations.js
@@ -0,0 +1,24 @@
+function increaseBalance(x, y) {
+  x = x + y;
+  return x;
+}
+
+function decreaseBalance(x, y) {
+  x = x - y;
+  return x;
+}
+
+function divideBalanceByAccounts(x, y, z) {
+  x = (x + z) / y;
+  return x;
+}
+
+function getRestAfterDivision(x, y) {
+  x = x % y;
+  return x;
+}
+
+console.log(increaseBalance(3000, 700));
+console.log(decreaseBalance(3000, 700));
+console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(getRestAfterDivision(7000, 3));
